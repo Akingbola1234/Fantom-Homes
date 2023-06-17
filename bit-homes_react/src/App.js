9ce054221b79e3460009e48d4853ab433d4c4389
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-17 13:27:08 +0100

    Started working on React Routes

--- bit-homes_react/src/App.js
+++ bit-homes_react/src/App.js
@@ -1,26 +1,40 @@
-import './App.css';
-import Creators from './Components/Creators/Creators';
-import Cta from './Components/Cta/Cta';
-import Footer from './Components/Footer/Footer';
-import HeroPage from './Components/HeroPage/HeroPage';
-import LiveAuction from './Components/LiveAuction/LiveAuction';
-import NavBar from './Components/NavBar/NavBar';
-import Partners from './Components/Partners/Partners';
-import SellNft from './Components/SellNft/SellNft';
-import SuperDrops from './Components/SuperDrops/SuperDrops';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Creators from "./Components/Creators/Creators";
+import Cta from "./Components/Cta/Cta";
+import Footer from "./Components/Footer/Footer";
+import HeroPage from "./Components/HeroPage/HeroPage";
+import LiveAuction from "./Components/LiveAuction/LiveAuction";
+import NavBar from "./Components/NavBar/NavBar";
+import Partners from "./Components/Partners/Partners";
+import SellNft from "./Components/SellNft/SellNft";
+import SuperDrops from "./Components/SuperDrops/SuperDrops";
 
 function App() {
   return (
     <div>
-      <NavBar/>
-      <HeroPage/>
-      <Partners/>
-      <SuperDrops/>
-      <LiveAuction/>
-      <SellNft/>
-      <Creators/>
-      <Cta/>
-      <Footer/>
+      <Router>
+      <NavBar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <HeroPage />
+                <Partners />
+                <SuperDrops />
+                <LiveAuction />
+                <SellNft />
+                <Creators />
+                <Cta />
+                <Footer />
+              </>
+            }
+          />
+          <Route exact path="marketplace" element={<SuperDrops/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
