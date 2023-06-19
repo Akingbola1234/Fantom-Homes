5a6b5a29461cd6be4d14b6ab484d63fb0f5e2142
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-19 13:57:44 +0100

    Updating HeroPage

--- fantom-homes_react/src/Components/HeroPage/HeroPage.jsx
+++ fantom-homes_react/src/Components/HeroPage/HeroPage.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Star from "../../Assets/images/Star.svg";
-import heroPageImage from "../../Assets/images/heropage-image.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import heroPageImage from "../../Assets/images/heropage-image-new.jpg";
 import arrowDown from "../../Assets/images/arrow-down.png";
 import "./HeroPage.css";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 const HeroPage = () => {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
   return (
     <div className="heropage-container">
       <div className="heropage-details">
@@ -41,11 +52,37 @@ const HeroPage = () => {
           </div>
         </div>
         <div className="heropage-image">
-          <img
-            src={heroPageImage}
-            alt="HeroPage Illustration"
-            className="heropage-img"
-          />
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            onAutoplayTimeLeft={onAutoplayTimeLeft}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src={heroPageImage} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={heroPageImage} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={heroPageImage} alt="" />
+            </SwiperSlide>
+            <div className="autoplay-progress" slot="container-end">
+              <svg viewBox="0 0 48 48" ref={progressCircle}>
+                <circle cx="24" cy="24" r="20"></circle>
+              </svg>
+              <span ref={progressContent}></span>
+            </div>
+          </Swiper>
         </div>
       </div>
     </div>
