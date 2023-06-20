0990d52aaff9dc65855b215f62fd5532e5364488
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-20 16:28:52 +0100

    Updated Collection Component

--- fantom-homes_react/src/Components/HomesCard/data.js
+++ fantom-homes_react/src/Components/HomesCard/data.js
@@ -3,13 +3,20 @@ import firstAppleImage from "../../Assets/homes-nfts/firstApple-Collection.jpg"
 import secondAppleImage from "../../Assets/homes-nfts/secondApple-Collection.jpg"
 import thirdAppleImage from "../../Assets/homes-nfts/thirdApple-Collection.jpg"
 import fourthAppleImage from "../../Assets/homes-nfts/fourthApple-Collection.jpg"
-
-
-
-
-import secondNFT from "../../Assets/homes-nfts/homes-nft-2.jpeg";
-import thirdNFT from "../../Assets/homes-nfts/homes-nft-3.jpeg";
-import fourthNFT from "../../Assets/homes-nfts/homes-nft-4.jpeg";
+//Male Trainer Collection
+import firstMaleTrainersImage from "../../Assets/homes-nfts/firstMale-Collection.jpg"
+import secondMaleTrainersImage from "../../Assets/homes-nfts/secondMale-Collection.jpg"
+import thirdMaleTrainersImage from "../../Assets/homes-nfts/thirdMale-Collection.jpg"
+import fourthMaleTrainersImage from "../../Assets/homes-nfts/fourthMale-Collection.jpg"
+//WristWatch
+import wristWatchImage from "../../Assets/homes-nfts/wristwatch-illustration.jpg"
+//Bracelet
+import braceletImage from "../../Assets/homes-nfts/bracelet.jpg"
+//Pants
+import vintagePants from "../../Assets/homes-nfts/vintage pants.jpg"
+import trousersPants from "../../Assets/homes-nfts/trousers.jpg"
+import ultraPants from "../../Assets/homes-nfts/ultra realistic.jpg"
+import geniePants from "../../Assets/homes-nfts/Genie pants.jpg"
 import fifthNFT from "../../Assets/homes-nfts/homes-nft-5.jpeg";
 import sixthNFT from "../../Assets/homes-nfts/homes-nft-6.jpeg";
 import seventhNFT from "../../Assets/homes-nfts/homes-nft-7.jpeg";
@@ -24,50 +31,50 @@ export const NFTs = [{
     },
     {
         key: 2,
-        nftName: "AvaxHomes 02",
-        nftImage: secondNFT,
+        nftName: "Male Collections",
+        nftImage: [firstMaleTrainersImage, secondMaleTrainersImage, thirdMaleTrainersImage, fourthMaleTrainersImage],
         nftPrice: "0.15",
         amount: "1 of 32",
     },
     {
         key: 3,
         nftName: "AvaxHomes 03",
-        nftImage: thirdNFT,
+        nftImage: [wristWatchImage, wristWatchImage, wristWatchImage, wristWatchImage],
         nftPrice: "0.15",
         amount: "1 of 32",
     },
     {
         key: 4,
         nftName: "AvaxHomes 04",
-        nftImage: fourthNFT,
+        nftImage: [braceletImage, braceletImage, braceletImage, braceletImage],
         nftPrice: "0.15",
         amount: "1 of 32",
     },
     {
         key: 5,
         nftName: "AvaxHomes 05",
-        nftImage: fifthNFT,
+        nftImage: [geniePants, geniePants, geniePants, geniePants],
         nftPrice: "0.15",
         amount: "1 of 32",
     },
     {
         key: 6,
         nftName: "AvaxHomes 06",
-        nftImage: sixthNFT,
+        nftImage: [trousersPants, trousersPants, trousersPants, trousersPants],
         nftPrice: "0.15",
         amount: "1 of 32",
     },
     {
         key: 7,
         nftName: "AvaxHomes 07",
-        nftImage: seventhNFT,
+        nftImage: [vintagePants, vintagePants, vintagePants, vintagePants],
         nftPrice: "0.15",
         amount: "1 of 32",
     },
     {
         key: 8,
         nftName: "AvaxHomes 08",
-        nftImage: eighthNFT,
+        nftImage: [ultraPants, ultraPants, ultraPants, ultraPants],
         nftPrice: "0.15",
         amount: "1 of 32",
     },
