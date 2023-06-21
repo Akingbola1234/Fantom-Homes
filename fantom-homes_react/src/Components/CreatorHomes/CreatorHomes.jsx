cc812b7ac4eeb6c7d37cdcbf9baf8a40a6f5be48
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-21 16:23:29 +0100

    Working on the Creator Homes Component

---
+++ fantom-homes_react/src/Components/CreatorHomes/CreatorHomes.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./CreatorHomes.css"
+
+const CreatorHomes = () => {
+  return (
+    <div className='creator-homes-container'>
+        <div className='creator-text-button'>
+            <div className='creator-text'>
+                <h5>My Homes</h5>
+                <span>0 RESULTS</span>
+            </div>
+            <div className='creator-buttons'>
+                <button>
+                    Upload Homes
+                </button>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default CreatorHomes
