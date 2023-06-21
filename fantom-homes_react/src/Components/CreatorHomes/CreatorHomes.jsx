a3fef32baf3486a17dedd6d1353b5ec8b0d811d3
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-21 16:29:51 +0100

    Working on the Creator Homes Component

--- fantom-homes_react/src/Components/CreatorHomes/CreatorHomes.jsx
+++ fantom-homes_react/src/Components/CreatorHomes/CreatorHomes.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import "./CreatorHomes.css"
+import React from "react";
+import "./CreatorHomes.css";
+import {AiOutlineUpload} from "react-icons/ai"
 
 const CreatorHomes = () => {
   return (
-    <div className='creator-homes-container'>
-        <div className='creator-text-button'>
-            <div className='creator-text'>
-                <h5>My Homes</h5>
-                <span>0 RESULTS</span>
-            </div>
-            <div className='creator-buttons'>
-                <button>
-                    Upload Homes
-                </button>
-            </div>
+    <div className="creator-homes-container">
+      <div className="creator-text-button">
+        <div className="creator-text">
+          <h5>My Homes</h5>
+          <span>0 RESULTS</span>
         </div>
-        
+        <div className="creator-button">
+          <button><AiOutlineUpload className="upload-icon"/> Upload Homes</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreatorHomes
+export default CreatorHomes;
