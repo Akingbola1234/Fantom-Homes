2ac823217f9f394ca8c3bf4674a064c2730f56d3
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-25 20:53:41 +0100

    Created the Nft Details page

--- app/pages/Components/NftDetailsPage/NftDetailsPage.jsx
+++ app/pages/Components/NftDetailsPage/NftDetailsPage.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import styles from "./NftDetailsPage.module.css"
+import React from "react";
+import { FaAngleLeft } from "react-icons/fa";
+import styles from "./NftDetailsPage.module.css";
 
 const NftDetailsPage = () => {
   return (
-    <div className=''>
-
+    <div className={styles["ntf-card-page-container"]}>
+      <div className={styles["ntf-card-page-details"]}>
+        <div className={styles["ntf-card-page-image"]}>
+          <img src="/Assets/images/3d-home.jpg" alt="NFT Image" />
+        </div>
+        <div className={styles["ntf-card-page-card"]}>
+          <button>
+            {" "}
+            <FaAngleLeft /> Back | FantomWorld
+          </button>
+          <h4>FantomHomes</h4>
+          <span>NFT ID </span>
+          <h5>Attributes: </h5>
+          <span>Owned by </span>
+          <button>Place a bid</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NftDetailsPage
+export default NftDetailsPage;
