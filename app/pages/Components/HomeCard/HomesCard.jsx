4fa159a436a7c12f11dd844fcd14e4d41aa05960
Olagboye Seyi <akingbolaola@gmail.com>
2023-07-02 16:20:22 +0100

    Working on the Readme.md

--- app/pages/Components/HomeCard/HomesCard.jsx
+++ app/pages/Components/HomeCard/HomesCard.jsx
@@ -33,6 +33,7 @@ const HomesCard = () => {
         router.push(`/page/NftDetails?${newModal.key}`)
     }
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isLoading, setLoading] = useState(true)
     const [modalContent, setModalContent] = useState([])
     // const [provider, setProvider] = useState(null)
     // const [NFTs, setNFTs] = useState([])
