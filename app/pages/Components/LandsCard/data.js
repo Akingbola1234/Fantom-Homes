05b69c0c0b2f329e1bedfac72fbfaa8be01ea864
Olagboye Seyi <akingbolaola@gmail.com>
2023-07-03 11:39:36 +0100

    Changed Ethereum Logo to Fantom Logo

--- app/pages/Components/LandsCard/data.js
+++ app/pages/Components/LandsCard/data.js
@@ -1,17 +1,7 @@
-//Apple Collection
-// import "./Assets/homes-nfts/firstLand.jpg" from "./Assets/homes-nfts/firstLand.jpg";
-// import "./Assets/homes-nfts/secondLand.jpg" from "./Assets/homes-nfts/secondLand.jpg";
-// import "./Assets/homes-nfts/thirdLand.jpg" from "./Assets/homes-nfts/thirdLand.jpg";
-// import "./Assets/homes-nfts/fourthLand.jpg" from "./Assets/homes-nfts/fourthLand.jpg";
-// import "./Assets/homes-nfts/fifthLand.jpg" from "./Assets/homes-nfts/fifthLand.jpg";
-// import "./Assets/homes-nfts/sixthLand.jpg" from "./Assets/homes-nfts/sixthLand.jpg";
-// import "./Assets/homes-nfts/seventhLand.jpg" from "./Assets/homes-nfts/seventhLand.jpg";
-// import "./Assets/homes-nfts/eighthLand.jpg" from "./Assets/homes-nfts/eighthLand.jpg";
-
 export const NFTs = [
     {
         key: 1,
-        nftName: "Apple Watch Collection",
+        nftName: "FantomWorld Land 01",
         nftImage: [
             "/Assets/homes-nfts/firstLand.jpg",
             "/Assets/homes-nfts/secondLand.jpg",
@@ -23,7 +13,7 @@ export const NFTs = [
     },
     {
         key: 2,
-        nftName: "Male Collections",
+        nftName: "FantomWorld Land 02",
         nftImage: [
             "/Assets/homes-nfts/secondLand.jpg",
             "/Assets/homes-nfts/fourthLand.jpg",
@@ -35,7 +25,7 @@ export const NFTs = [
     },
     {
         key: 3,
-        nftName: "Wrist Watch Collection",
+        nftName: "FantomWorld Land 03",
         nftImage: [
             "/Assets/homes-nfts/thirdLand.jpg",
             "/Assets/homes-nfts/fourthLand.jpg",
@@ -47,7 +37,7 @@ export const NFTs = [
     },
     {
         key: 4,
-        nftName: "Bracelet Collection",
+        nftName: "FantomWorld Land 04",
         nftImage: [
             "/Assets/homes-nfts/fourthLand.jpg",
             "/Assets/homes-nfts/fifthLand.jpg",
@@ -59,7 +49,7 @@ export const NFTs = [
     },
     {
         key: 5,
-        nftName: "Genie Pants Collection",
+        nftName: "FantomWorld Land 05",
         nftImage: [
             "/Assets/homes-nfts/fifthLand.jpg",
             "/Assets/homes-nfts/seventhLand.jpg",
@@ -71,7 +61,7 @@ export const NFTs = [
     },
     {
         key: 6,
-        nftName: "Shirts Collection",
+        nftName: "FantomWorld Land 06",
         nftImage: [
             "/Assets/homes-nfts/sixthLand.jpg",
             "/Assets/homes-nfts/fifthLand.jpg",
@@ -83,7 +73,7 @@ export const NFTs = [
     },
     {
         key: 7,
-        nftName: "Vintage Pants Collection",
+        nftName: "FantomWorld Land 07",
         nftImage: [
             "/Assets/homes-nfts/seventhLand.jpg",
             "/Assets/homes-nfts/eighthLand.jpg",
@@ -95,7 +85,7 @@ export const NFTs = [
     },
     {
         key: 8,
-        nftName: "Ultra Pants Collection",
+        nftName: "FantomWorld Land 08",
         nftImage: [
             "/Assets/homes-nfts/eighthLand.jpg",
             "/Assets/homes-nfts/fifthLand.jpg",
