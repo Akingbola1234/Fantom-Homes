538aa07c877af316376b885b4b0c08c15ffa4ae3
Olagboye Seyi <akingbolaola@gmail.com>
2023-07-03 12:49:55 +0100

    Reolving Deployment issues

---
+++ app/superDropData/data.js
@@ -0,0 +1,21 @@
+import { Tag } from "antd"
+import CollectionsCard from "../pages/Components/CollectionsCard/CollectionsCard"
+import LandsCard from "../pages/Components/LandsCard/LandsCard"
+import HomesCard from "../pages/Components/HomeCard/HomeCard"
+export const ourTabs = [
+    {
+        key: 1,
+        Tab: <Tag className="tags">Homes</Tag>,
+        children: <HomesCard />,
+    },
+    {
+        key: 2,
+        Tab: <Tag className="tags">Collections</Tag>,
+        children: <CollectionsCard />,
+    },
+    {
+        key: 3,
+        Tab: <Tag className="tags">Lands</Tag>,
+        children: <LandsCard />,
+    },
+]
